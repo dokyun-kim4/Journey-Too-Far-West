edb890911108dcc9d518957aa4c21ce99f7f5123
Dokyun Kim <donny5557@gmail.com>
2024-03-30 19:02:28 -0400

    implement car appearing upon stage clear

--- src/maps/brooklyn.gd
+++ src/maps/brooklyn.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 var car_activate = false
-const MAX_MOBS = 20
+var stage_clear = false
+
+const MAX_MOBS = 2
+var mob_count = 0
 var killed = 0
 
+func _ready():
+	%car.hide()
+
 func _physics_process(delta):
-	if car_activate == true:
+	if car_activate == true and stage_clear == true:
 		if Input.is_action_just_pressed("next_level"):
 			var next_scene = load("res://src/maps/dallas.tscn")
 			get_tree().change_scene_to_packed(next_scene)
@@ -25,12 +31,15 @@ func _on_area_2d_body_exited(body):
 	car_activate = false
 	
 func _on_timer_timeout():
-	print("Mob Spawned")
-	spawn_mob()
-	%SpawnTimer.wait_time = randf_range(2.0, 5.0)
-	print("Time until next spawn: ", %SpawnTimer.wait_time)
+	if mob_count < MAX_MOBS:
+		spawn_mob()
+		%SpawnTimer.wait_time = randf_range(2.0, 5.0)
+		print("Time until next spawn: ", %SpawnTimer.wait_time)		
 
 func _on_mob_dead():
-	print("BRRRRRRRR")
+	killed += 1
+	if killed == MAX_MOBS:
+		%car.show()
+		stage_clear = true
 
 
