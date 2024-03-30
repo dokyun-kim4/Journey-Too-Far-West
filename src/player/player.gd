bb87d1c5cea75c9bf0136329b2000033cdcad468
eys123 <esun@olin.edu>
2024-03-30 14:28:58 -0400

    add player health bar

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
+signal player_dead
+
+var health = 100.0
+var cur_facing = Vector2.RIGHT
+
 const PLAYER_BASE_SPEED = 600
 const SPRINT_MULTIPLIER = 2
-var cur_facing = Vector2(1,0)
+const DAMAGE_RATE = 5.0
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -17,5 +22,15 @@ func _physics_process(delta):
 		velocity = cur_facing * PLAYER_BASE_SPEED * SPRINT_MULTIPLIER
 	else: 
 		velocity = direction * PLAYER_BASE_SPEED 
-
 	move_and_slide()
+	
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%HealthBar.value = health
+	
+	if health <= 0.0:
+		player_dead.emit()
+
+	
+		
