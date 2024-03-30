5117a1183d7bce68337add3e02348a7f592e1e9f
eys123 <esun@olin.edu>
2024-03-30 13:34:43 -0400

    initialize bullet

---
+++ src/bullet/bullet.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+const BULLET_SPEED = 1000
+const BULLET_RANGE = 1200
+var travelled_dist = 0;
+
+func _physics_process(delta):
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction * delta
+	travelled_dist += BULLET_SPEED * delta
+	
+	if travelled_dist > BULLET_RANGE:
+		queue_free()
--- src/gun/gun.gd
+++ src/gun/gun.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
 func _physics_process(delta):
-	var targets_in_range = get_overlapping_bodies()
-	if targets_in_range.size() > 0:
-		var cur_target = targets_in_range[0]
-		look_at(cur_target.global_position)
+	look_at(get_global_mouse_position())
+	#var targets_in_range = get_overlapping_bodies()
+	#if targets_in_range.size() > 0:
+		#var cur_target = targets_in_range[0]
+		#look_at(cur_target.global_position)
+
+func shoot():
+	var new_bullet = preload("res://src/bullet/bullet.tscn").instantiate()
+	new_bullet.global_position = %ShootPoint.global_position
+	%ShootPoint.add_child(new_bullet)
