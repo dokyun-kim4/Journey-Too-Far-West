2b99f6fd2bfceae2b800432f9b8abdc23abb2f4f
eys123 <esun@olin.edu>
2024-03-30 14:20:03 -0400

    timer add fix?

--- src/gun/gun.gd
+++ src/gun/gun.gd
@@ -5,7 +5,7 @@ func _physics_process(delta):
 	#var targets_in_range = get_overlapping_bodies()
 	#if targets_in_range.size() > 0:
 		#var cur_target = targets_in_range[0]
-		#look_at(cur_target.global_position)
+		#look_at(cur_target.global_position) s
 	if Input.is_action_pressed("shoot"):
 		%ShootTimer.start(0.05)
 	
