f7c7b61bbdac714cc29588f1c5932ddb17d56185
Dokyun Kim <donny5557@gmail.com>
2024-03-30 16:06:08 -0400

    implement jeep in dallas map

--- src/maps/brooklyn.gd
+++ src/maps/brooklyn.gd
@@ -3,14 +3,20 @@ signal go_to_dallas
 
 var entered = false
 
-func _on_area_2d_body_entered(body: CharacterBody2D):
-	entered = true	
-
-func _on_area_2d_body_exited(body: CharacterBody2D):
+func _on_area_2d_body_entered(body):
+	entered = true
+	
+func _on_area_2d_body_exited(body):
 	entered = false
-
+	
 func _physics_process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("next_level"):
 			var next_scene = load("res://src/maps/dallas.tscn")
 			get_tree().change_scene_to_packed(next_scene)
+
+
+
+
+
+
--- src/maps/dallas.gd
+++ src/maps/dallas.gd
@@ -2,13 +2,19 @@ extends Node2D
 var entered = false
 
 func _on_area_2d_body_entered(body: CharacterBody2D):
-	entered = true	
-
+	entered = true
+	
 func _on_area_2d_body_exited(body: CharacterBody2D):
 	entered = false
 
 func _physics_process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("next_level"):
-			var next_scene = load("res://src/maps/brooklyn.tscn")
+			var next_scene = load("res://src/maps/los_angeles.tscn")
 			get_tree().change_scene_to_packed(next_scene)
+
+
+
+
+
+
