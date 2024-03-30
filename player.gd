139630a7e68baa6e3592fc632b01232b35e42290
eys123 <esun@olin.edu>
2024-03-30 12:23:13 -0400

    init player weapon

--- player.gd
+++ player.gd
@@ -5,4 +5,6 @@ const PLAYER_BASE_SPEED = 600
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * PLAYER_BASE_SPEED
+	#if abs(direction_x) >= 0.001:
+		#scale.x = sign(direction_x)
 	move_and_slide()
