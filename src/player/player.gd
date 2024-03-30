e7738d719b87fea17858124b8555ec9136c45117
eys123 <esun@olin.edu>
2024-03-30 13:10:05 -0400

    delete temp wukong, actually fix sprint

--- src/player/player.gd
+++ src/player/player.gd
@@ -2,11 +2,11 @@ extends CharacterBody2D
 
 const PLAYER_BASE_SPEED = 600
 const SPRINT_MULTIPLIER = 2
-var cur_facing = 1
+var cur_facing = Vector2(1,0)
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if abs(direction.x) > 0:
+	if abs(direction.x) > 0 or abs(direction.y) > 0:
 		cur_facing = direction
 	
 	if Input.is_action_pressed("sprint"):
